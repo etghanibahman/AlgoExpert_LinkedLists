0f8492e1f2021d206cade8a78899453fdaeab1cb
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-05-04 00:20:53 +0200

    Sum of linked list- new node

--- SumOfLinkedLists/Program.cs
+++ SumOfLinkedLists/Program.cs
@@ -36,41 +36,90 @@ namespace SumOfLinkedLists
         }
 
 
+        #region My_Solution
         public static LinkedList SumOfLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int loop = 0;
-            while (linkedListOne != null)
+            var linkedListOneCurrentNode = linkedListOne;
+            var linkedListTwoCurrentNode = linkedListTwo;
+            LinkedList sumOfLinkedLists = null;
+            LinkedList currentNode = null;
+            var num1 = 0;
+            var num2 = 0;
+            var carryOver = 0;
+            while (linkedListOneCurrentNode != null || linkedListTwoCurrentNode != null || carryOver > 0)
             {
-                num1 += linkedListOne.value * int.Parse(Math.Pow(10,loop).ToString());
-                loop += 1;
-                linkedListOne = linkedListOne.next;
+                num1 = 0;
+                num2 = 0;
+                if (linkedListOneCurrentNode != null)
+                {
+                    num1 = linkedListOneCurrentNode.value;
+                    linkedListOneCurrentNode = linkedListOneCurrentNode.next;
+                }
+                if (linkedListTwoCurrentNode != null)
+                {
+                    num2 = linkedListTwoCurrentNode.value;
+                    linkedListTwoCurrentNode = linkedListTwoCurrentNode.next;
+                }
+                var total = num1 + num2 + carryOver;
+                var numberForSumOfLinkedLists = 0;
+                numberForSumOfLinkedLists = total % 10;
+                carryOver = total / 10;
+                var newNode = new LinkedList(numberForSumOfLinkedLists);
+                if (sumOfLinkedLists == null)
+                {
+                    sumOfLinkedLists = newNode;
+                    currentNode = sumOfLinkedLists;
+                }
+                else
+                {
+                    currentNode.next = newNode;
+                    currentNode = currentNode.next;
+                }
             }
-            loop = 0;
-            while (linkedListTwo != null)
-            {
-                num2 += linkedListTwo.value * int.Parse(Math.Pow(10, loop).ToString());
-                loop += 1;
-                linkedListTwo = linkedListTwo.next;
-            }
-
-            
-            LinkedList result = new LinkedList((num1 + num2) % 10);
-            if (num1 + num2 < 10)
-            {
-                return result;
-            }
-            var sum = ((num1 + num2)/10).ToString();
-            LinkedList cur = result;
-            for (int i = sum.Length - 1; i >= 0; i--)
-            {
-                cur.next = new LinkedList(int.Parse(sum[i].ToString()));
-                cur = cur.next;
-            }
-
-            return result;
+    
+            return sumOfLinkedLists;
         }
+
+        #endregion
+
+        #region My__Solution
+        //public static LinkedList SumOfLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo)
+        //{
+        //    int num1 = 0;
+        //    int num2 = 0;
+        //    int loop = 0;
+        //    while (linkedListOne != null)
+        //    {
+        //        num1 += linkedListOne.value * int.Parse(Math.Pow(10, loop).ToString());
+        //        loop += 1;
+        //        linkedListOne = linkedListOne.next;
+        //    }
+        //    loop = 0;
+        //    while (linkedListTwo != null)
+        //    {
+        //        num2 += linkedListTwo.value * int.Parse(Math.Pow(10, loop).ToString());
+        //        loop += 1;
+        //        linkedListTwo = linkedListTwo.next;
+        //    }
+
+
+        //    LinkedList result = new LinkedList((num1 + num2) % 10);
+        //    if (num1 + num2 < 10)
+        //    {
+        //        return result;
+        //    }
+        //    var sum = ((num1 + num2) / 10).ToString();
+        //    LinkedList cur = result;
+        //    for (int i = sum.Length - 1; i >= 0; i--)
+        //    {
+        //        cur.next = new LinkedList(int.Parse(sum[i].ToString()));
+        //        cur = cur.next;
+        //    }
+
+        //    return result;
+        //}
+        #endregion
+
 
         // This is an input class. Do not edit.
         public class LinkedList
